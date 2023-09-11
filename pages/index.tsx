9c420be62e6b8d62cd0f81fa18c111239cd10cbc
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-09-12 04:29:16 +0800

    add pixel ratio dynamic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,9 +12,37 @@ export default function Home() {
     codeUrl: "build/myunityapp.wasm",
   });
 
+  // We'll use a state to store the device pixel ratio.
+  const [devicePixelRatio, setDevicePixelRatio] = useState(
+    window.devicePixelRatio
+  );
+
   // We'll round the loading progression to a whole number to represent the
   // percentage of the Unity Application that has loaded.
   const loadingPercentage = Math.round(loadingProgression * 100);
+
+  useEffect(
+    function () {
+      // A function which will update the device pixel ratio of the Unity
+      // Application to match the device pixel ratio of the browser.
+      const updateDevicePixelRatio = function () {
+        setDevicePixelRatio(window.devicePixelRatio);
+      };
+      // A media matcher which watches for changes in the device pixel ratio.
+      const mediaMatcher = window.matchMedia(
+        `screen and (resolution: ${devicePixelRatio}dppx)`
+      );
+      // Adding an event listener to the media matcher which will update the
+      // device pixel ratio of the Unity Application when the device pixel
+      // ratio changes.
+      mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+      return function () {
+        // Removing the event listener when the component unmounts.
+        mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+      };
+    },
+    [devicePixelRatio]
+  );
 
   return (
     <div className="container">
@@ -24,7 +53,11 @@ export default function Home() {
           <p>Loading... ({loadingPercentage}%)</p>
         </div>
       )}
-      <Unity className="unity" unityProvider={unityProvider} />
+      <Unity
+        className="unity"
+        unityProvider={unityProvider}
+        devicePixelRatio={devicePixelRatio}
+      />
     </div>
   );
 }
